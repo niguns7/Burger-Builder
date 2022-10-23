d3d1024024da033c36a8280cd2d94d772bba9b50
nirguns7 <nirja767@gmail.com>
2022-10-23 13:01:22 +0545

    transformed object into array

--- src/components/Layout/Burger/Burger.js
+++ src/components/Layout/Burger/Burger.js
@@ -1,14 +1,29 @@
 import React from "react";
 import Burgering from "./Burgering/Burgering";
-import classes from "./Burger.css"
+import classes from "./Burger.module.css"
 
 const burger = (props) => {
-    return(
+    // transforming object into array
+    let transformedingredients = Object.keys(props.ingredients)
+    .map(igkey => {
+        //creating an array
+        return [...Array( props.ingredients[igkey] )].map(( _ , i)=> {
+            return <Burgering key={igkey + i} type={igkey} />
+        });
+    })
+    .reduce ((arr,el) => {
+        return arr.concat(el)
+    }, []);
+
+    if (transformedingredients.length === 0) {
+        transformedingredients = <p> Please start adding ingrediens!</p>
+    }
+    
+    return (
         <div className={classes.Burger}>
-            <Burgering />
-            <Burgering />
-            <Burgering />
-            <Burgering />
+            <Burgering type="bread-top" />
+            {transformedingredients}
+            <Burgering type="bread-bottom" />
         </div>
     );
 
