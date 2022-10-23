859f6eba65b082fd46fe44db8fd41443d8bc6ab5
nirguns7 <nirja767@gmail.com>
2022-10-23 13:03:24 +0545

    component added on main component

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,13 +1,24 @@
 import React, { Component } from "react";
 import Auxl from "../../hoc/Auxl";
 import Burger from "../../components/Layout/Burger/Burger";
-
+import BuildContrls from "../../components/Layout/Burger/BuildControls/BuildContrls";
 class BurgerBuilder extends Component {
+
+    state = {
+        ingredients: {
+            salad: 0,
+            bacon: 0,
+            cheese: 0,
+            meat : 0
+
+        }
+    }
+
     render() {
         return (
             <Auxl> 
-                <Burger />
-                <div>Build Controls</div>
+                <Burger ingredients={this.state.ingredients}/>
+                <BuildContrls />
             </Auxl>
         );
     }
