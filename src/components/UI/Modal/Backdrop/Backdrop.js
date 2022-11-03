7f1b26abf4a2a9b3808d5e76866f8e00b75745cf
nirguns <nirja767@gmail.com>
2022-11-03 18:34:06 +0545

    Black gradient

--- src/components/UI/Modal/Backdrop/Backdrop.js
+++ src/components/UI/Modal/Backdrop/Backdrop.js
@@ -1,8 +1,8 @@
 import React from "react";
+import classes from "./Backdrop.module.css";
 
-
-const backdrop = (props) => (
-    props.show ? <div></div> :null
+const Backdrop = (props) => (
+    props.show ? <div className={classes.Backdrop} onClick= {props.clicked}></div> :null
 );
 
-export default backdrop;
+export default Backdrop;
