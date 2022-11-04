456fc1c817e3d805038810cfd6b7e17944c912bb
nirguns <nirja767@gmail.com>
2022-11-04 15:52:59 +0545

    cancelling and continuing the process

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -76,6 +76,13 @@ class BurgerBuilder extends Component {
         }
         );
     }
+    purchaseCancelHandler = () => {
+        this.setState({purchasing: false});
+    }
+
+    purchaseContinueHandler = () => {
+        alert('you continue!');
+    }
     render() {
         const disabledInfo = {
             ...this.state.ingredients
@@ -86,7 +93,10 @@ class BurgerBuilder extends Component {
         return (
             <Auxl> 
                 <Modal show={this.state.purchasing} modalclosed>
-                    <OrderSummary ingredients={this.state.ingredients}/>
+                    <OrderSummary 
+                        ingredients={this.state.ingredients}
+                        purchaseCancled={this.purchaseCancelHandler}
+                        PurchaseContinued={this.purchaseContinueHandler}/>
                 </Modal>
                 <Burger ingredients={this.state.ingredients}/>
                 <BuildContrls
